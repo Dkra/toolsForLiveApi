1e9bbbd6c85a0d4711ede2b024f231b67e60b439
Roger Jiang <dkra4741@gmail.com>
2017-10-30 14:46:35 +0800

    add black-list api, black-list.json file to record which object do not show

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,39 @@ const Router = require('koa-router');
 const router = new Router();
 const path = require('path');
 const bluebird = require('bluebird');
-var cmd = require('node-cmd');
+const cmd = require('node-cmd');
+const fs = require('fs');
 
+router.get('/search591/black-list', (ctx) => {
+  try {
+    const rs = fs.readFileSync('black-list.json', 'utf8');
+    return ctx.body = JSON.parse(rs);
+
+  } catch(err) {
+    console.log('err', err)
+    return ctx.status = 500
+  }
+})
+
+router.post('/search591/black-list/add', (ctx) => {
+  const { id } = ctx.request.body;
+  const rs = fs.readFileSync('black-list.json', 'utf8');
+  const newBlackList = [...JSON.parse(rs), id]
+  
+  fs.writeFile('black-list.json', JSON.stringify(newBlackList), function (err) {
+    if (err)
+      console.log(err);
+    else
+      console.log('Write operation complete.');
+      
+  });
+  // always success?
+  return ctx.status = 200
+})
 
 router.get('/search591', async (ctx) => {
   // check 591 rsList Api request url
-  const url = 'https://rent.591.com.tw/home/search/rsList\?is_new_list\=1\&type\=1\&kind\=2\&searchtype\=1\&region\=8\&section\=102,103,101\&rentprice\=8000,10000\&area\=11,18'
+  const url = 'https://rent.591.com.tw/home/search/rsList\?is_new_list\=1\&type\=1\&kind\=2\&searchtype\=1\&region\=8\&section\=102,103,101\&rentprice\=7499,10000\&area\=10,17'
   try {
     const getCommand = (firstRow) => {
       return `curl -X GET '${url}&firstRow=${firstRow}' -H 'cookie: PHPSESSID=rgnmg9mnfa9gu5chdco1ds9ci7; new_rent_list_kind_test=1; c10f3143a018a0513ebe1e8d27b5391c=1; _ga=GA1.3.2085218089.1509093321; _gid=GA1.3.215660181.1509093321; urlJumpIp=8; urlJumpIpByTxt=%E5%8F%B0%E4%B8%AD%E5%B8%82; 591_new_session=eyJpdiI6Ik5UaUhZcUhaajl2b201ZFRiOW5VZ2c9PSIsInZhbHVlIjoidHg3TWFwNFJUK1kwRFlJOHBZV1lhUkxYYjhobk1yV05INHJqNFFUMWpySTlWXC9HVVRSbFU4Z1p0Ryt6RlNcL1ZMdFwvZG9FaEFuYXRwcnZ0OERvamI4QXc9PSIsIm1hYyI6IjY0Nzc5ZDFkNTNmMGQyODg2ZDVjMzYzMjVjNWVhNDRhZjlkYTE4ZjkxNTg3ODU5YzFmY2U5MjdhMWM4YmUxNjEifQ%3D%3D; _ga=GA1.4.2085218089.1509093321; _gid=GA1.4.215660181.1509093321; _gat_UA-97423186-1=1'`
