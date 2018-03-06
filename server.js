d14b10c55ee4bd988a3c30494ef97dc5b6111368
roger.jiang <roger.jiang@afusion.com>
2018-03-07 00:32:23 +0800

    rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const Koa = require('koa')
+const router = require('./routes')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const json = require('koa-json')
+const cors = require('koa2-cors')
+
+const port = process.env.PORT || 8888
+
+const app = new Koa()
+
+// logger middleware
+app.use(logger())
+
+// add bodyParser middleware
+app.use(bodyparser())
+
+// Json prettier
+app.use(json())
+
+// allow cors
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'PUT', 'POST'],
+		headers: ['Content-Type', 'Authorization']
+	})
+)
+
+// add router middleware:
+app.use(router.routes(), router.allowedMethods())
+
+// Listen on Port
+app.listen(port)
